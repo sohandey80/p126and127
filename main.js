e58b204b21140541d15022c0feccdd8a8908b1fc
sohandey80 <77267242+sohandey80@users.noreply.github.com>
2023-06-08 11:48:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song = "";
 music ="";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWrsitY = 0;
 
 function preload()
 {
@@ -14,9 +18,35 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose' , gotPoses );
+}
+
+function modelLoaded()
+{
+  console.log('poseNet is initialized');
+}
+
+
+function gotPoses(results)
+{
+  if (results.length>0) {
+    console.log(results);
+    leftWristX = results[0].pose.leftWRist.x;
+        leftWristY = results[0].pose.leftWRist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+  }
 }
 
 function draw()
 {
     image(video,0,0,600,500);
+    fill("#ff0000");
+    stroke("#ff0000");
 }
+
